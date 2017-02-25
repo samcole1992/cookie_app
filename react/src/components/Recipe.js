999932d9d151f4f132b14acdd1c71fa187759d88
Samuel Cole <samcole1992@gmail.com>
2017-02-24 22:10:39 -0500

    added Recipe React Tree

---
+++ react/src/components/Recipe.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+class Recipe extends Component {
+  constructor(props){
+    super(props);
+    this.state: {
+      recipe: null
+    };
+this.handleRadioChange = this.handleRadioChange.bind(this)
+  }
+
+  handleRadioChange = (event)=>{
+    this.setState({
+      recipe: event.currentTarget.value
+    })
+  }
+
+  componentDidMount(){
+    let key = ENV["FOOD2FORK_KEY"]
+    let type = '';
+    if (this.state.recipe== chocolateChip) {
+      type = 'chocolate%20chip%20'
+    }
+    else if (this.state.recipe== peanutButter) {
+      type = 'peanut%20butter%20'
+    }
+    else if (this.state.recipe == sugar) {
+      type = 'sugar%20'
+    }
+    else if (this.state.recipe == oatmealRaisin) {
+      type = 'oatmeal%20raisin%20'
+    }
+    else if (this.state.recipe == snickerDoodle) {
+      type = 'snickerdoodle%20'
+    }
+    else if (this.state.recipe== gingerbread) {
+      type = 'gingerbread%20'
+    }
+    else if (this.state.recipe== shortbread) {
+      type = 'shortbread'
+    }
+    else {
+      type = ''
+    }
+    }
+    fetch(`http://food2fork.com/api/search?key=${key}&q=${}cookies`)
+  }
+
+}
