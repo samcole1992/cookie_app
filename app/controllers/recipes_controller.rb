712cac3338bc5217cbf00b33b5a28af7137ce0c1
Samuel Cole <samcole1992@gmail.com>
2017-03-12 23:06:40 -0400

    Progress on user/recipe react

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
   def index
     @user = current_user
 
+
   end
 
 end
--- react/src/components/RecipeList.js
+++ react/src/components/RecipeList.js
@@ -7,8 +7,8 @@ class RecipeList extends Component {
     this.state= {
       recipes:[],
       recipe: null,
-      type:'chocolate%20chip%20',
-      key:ENV["FOOD2FORK_KEY"]
+      type:'',
+      key:'06560de398a0c7f695ec038ce5ad9927'
     };
 this.handleOptionChange = this.handleOptionChange.bind(this)
   }
@@ -19,47 +19,49 @@ this.handleOptionChange = this.handleOptionChange.bind(this)
   });
 }
 
-  componentWillMount(){
-    if (this.state.recipe== chocolateChip) {
-      this.setState({
-      type: 'chocolate%20chip%20'})
-    }
-    else if (this.state.recipe== peanutButter) {
-      this.setState({
-      type: 'peanut%20butter%20'})
-        }
-    else if (this.state.recipe == sugar) {
-      this.setState({
-      type: 'sugar'})
-        }
-    else if (this.state.recipe == oatmealRaisin) {
-      this.setState({
-      type:'oatmeal%20raisin%20'
-    })
-    }
-    else if (this.state.recipe == snickerDoodle) {
-      this.setState({
-
-      type:'snickerdoodle%20'
-    })
-    }
-    else if (this.state.recipe== gingerbread) {
-      this.setState({
-
-      type:'gingerbread%20'})
-
-    }
-    else if (this.state.recipe== shortbread) {
-      this.setState({
-
-      type:'shortbread%20'
-    })
-
-    }
+  componentDidMount(){
+    // debugger;
+    // if (this.state.recipe== chocolateChip) {
+    //   this.setState({
+    //   type: 'chocolate%20chip%20'})
+    // }
+    // else if (this.state.recipe== peanutButter) {
+    //   this.setState({
+    //   type: 'peanut%20butter%20'})
+    //     }
+    // else if (this.state.recipe == sugar) {
+    //   this.setState({
+    //   type: 'sugar'})
+    //     }
+    // else if (this.state.recipe == oatmealRaisin) {
+    //   this.setState({
+    //   type:'oatmeal%20raisin%20'
+    // })
+    // }
+    // else if (this.state.recipe == snickerDoodle) {
+    //   this.setState({
+    //
+    //   type:'snickerdoodle%20'
+    // })
+    // }
+    // else if (this.state.recipe== gingerbread) {
+    //   this.setState({
+    //
+    //   type:'gingerbread%20'})
+    //
+    // }
+    // else if (this.state.recipe== shortbread) {
+    //   this.setState({
+    //
+    //   type:'shortbread%20'
+    // })
+    //
+    // }
     fetch(`http://food2fork.com/api/search?key=${this.state.key}&q=${this.state.type}cookies`,{
       method: 'get'
     })
     .then(response => {
+      debugger;
       if (response.ok) {
         return response;
       }
@@ -98,10 +100,75 @@ this.handleOptionChange = this.handleOptionChange.bind(this)
     })
     recipes;
     return(
-
- <div>
-{recipes}
- </div>
+  <form>
+    <div className="radio">
+      <label>
+        <input type="radio" value="chocolateChip"
+                     checked={this.state.recipe === 'chocolateChip'}
+                     onChange={this.handleOptionChange} />
+                Chocolate Chip
+      </label>
+    </div>
+    <div className="radio">
+     <label>
+       <input type="radio" value="peanutButter"
+                     checked={this.state.recipe === 'peanutButter'}
+                     onChange={this.handleOptionChange} />
+       Peanut Butter
+      </label>
+    </div>
+    <div className="radio">
+     <label>
+       <input type="radio" value="sugar"
+                     checked={this.state.recipe === 'sugar'}
+                     onChange={this.handleOptionChange} />
+       Sugar
+     </label>
+    </div>
+    <div className="radio">
+     <label>
+       <input type="radio" value="oatmealRaisin"
+                     checked={this.state.recipe === 'oatmealRaisin'}
+                     onChange={this.handleOptionChange} />
+       Oatmeal Raisin
+     </label>
+    </div>
+    <div className="radio">
+     <label>
+       <input type="radio" value="snickerDoodle"
+                     checked={this.state.recipe === 'snickerDoodle'}
+                     onChange={this.handleOptionChange} />
+       SnickerDoodle
+     </label>
+    </div>
+   <div className="radio">
+     <label>
+       <input type="radio" value="gingerbread"
+                     checked={this.state.recipe === 'gingerbread'}
+                     onChange={this.handleOptionChange} />
+       Gingerbread
+     </label>
+   </div>
+   <div className="radio">
+     <label>
+       <input type="radio" value="shortbread"
+                     checked={this.state.recipe === 'shortbread'}
+                     onChange={this.handleOptionChange} />
+       Shortbread
+     </label>
+   </div>
+   <div className="radio">
+     <label>
+       <input type="radio" value=""
+                     checked={this.state.recipe === ''}
+                     onChange={this.handleOptionChange} />
+       Random
+     </label>
+   </div>
+ </form>
+//  <div>
+// {recipes}
+//  </div>
 
     )
   }
