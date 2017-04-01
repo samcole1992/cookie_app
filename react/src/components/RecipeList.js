89741385f65958feab35f85c77f640b9313de21d
Samuel Cole <samcole1992@gmail.com>
2017-03-31 22:28:33 -0400

    working on React update

--- react/src/components/RecipeList.js
+++ react/src/components/RecipeList.js
@@ -17,70 +17,71 @@ this.handleOptionChange = this.handleOptionChange.bind(this)
   this.setState({
     recipe: changeEvent.target.value
   });
-  if (this.state.recipe== "chocolateChip") {
-    this.setState({
-    type: 'chocolate%20chip%20'})
-  }
-  else if (this.state.recipe== "peanutButter") {
-    this.setState({
-    type: 'peanut%20butter%20'})
-      }
-  else if (this.state.recipe == "sugar") {
-    this.setState({
-    type: 'sugar%20'})
-      }
-  else if (this.state.recipe == "oatmealRaisin") {
-    this.setState({
-    type:'oatmeal%20raisin%20'
-  })
-  }
-  else if (this.state.recipe == "snickerDoodle") {
-    this.setState({
-
-    type:'snickerdoodle%20'
-  })
-  }
-  else if (this.state.recipe== "gingerbread") {
-    this.setState({
-
-    type:'gingerbread%20'})
-
-  }
-  else if (this.state.recipe== "shortbread") {
-    this.setState({
-
-    type:'shortbread%20'
-  })
-  }
-  console.log(this.state);
 }
 
-  componentWillUpdate(){
 
-    fetch(`http://food2fork.com/api/search?key=${this.state.key}&q=${this.state.type}cookies`,{
+  componentDidUpdate(){
+    debugger;
+    if (this.state != saveState) {
 
-    })
-    .then(response => {
-      if (response.ok) {
-        return response;
-      }
-      else {
-        let errorMessage = `${response.status}, (${response.statusText})`;
-        let error = new Error(errorMessage);
-        throw(error);
-      }
-    })
-    .then(response => response.json())
-    .then(response=> {
-      let newRecipes =[];
-      debugger;
-      response.forEach(function(recipe){
-        newRecipes.push(recipe);
-      });
+
+    if (this.state.recipe== "chocolateChip") {
       this.setState({
-        recipes: newRecipes
-      });
-    });
+      type: 'chocolate%20chip%20'})
+    }
+    else if (this.state.recipe== "peanutButter") {
+      this.setState({
+      type: 'peanut%20butter%20'})
+        }
+    else if (this.state.recipe == "sugar") {
+      this.setState({
+      type: 'sugar%20'})
+        }
+    else if (this.state.recipe == "oatmealRaisin") {
+      this.setState({
+      type:'oatmeal%20raisin%20'
+    })
+    }
+    else if (this.state.recipe == "snickerDoodle") {
+      this.setState({
+
+      type:'snickerdoodle%20'
+    })
+    }
+    else if (this.state.recipe== "gingerbread") {
+      this.setState({
+
+      type:'gingerbread%20'})
+
+    }
+    else if (this.state.recipe== "shortbread") {
+      this.setState({
+
+      type:'shortbread%20'
+    })
+  }
+    //
+    // fetch(`http://food2fork.com/api/search?key=${this.state.key}&q=${this.state.type}cookies`,{
+    //
+    // })
+    // .then(response => {
+    //   if (response.ok) {
+    //     return response;
+    //   }
+    //   else {
+    //     let errorMessage = `${response.status}, (${response.statusText})`;
+    //     let error = new Error(errorMessage);
+    //     throw(error);
+    //   }
+    // })
+    // .then(response => response.json())
+    // .then(response=> {
+    //   this.setState({
+    //     recipes: response.recipes
+    //   });
+    // });
+    console.log(this.state);
+  }
   }
 
   render(){
